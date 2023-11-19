e894f97c7bff693c6539d740b0ee4cc75425a910
hfidelis <heitorc88@gmail.com>
2023-11-19 20:55:23 -0300

    feat: added People service

---
+++ src/app/services/people/people.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { People } from '../../models/People.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeopleService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  private baseUrl: string = 'http://localhost:3000/people'
+
+  getPeople(): Observable<People[]> {
+    return this.http.get<People[]>(this.baseUrl)
+  }
+
+  getPeopleById(id: number | string): Observable<People> | undefined {
+    return this.http.get<People>(`${this.baseUrl}/${id}`)
+  }
+
+  addPeople(people: People): Observable<People> {
+    return this.http.post<People>(this.baseUrl, people)
+  }
+
+  updatePeople(people: People): Observable<People> {
+    return this.http.patch<People>(`${this.baseUrl}/${people.id}`, people)
+  }
+
+  deletePeople(id: number | string): Observable<People> {
+    return this.http.delete<People>(`${this.baseUrl}/${id}`)
+  }
+}
