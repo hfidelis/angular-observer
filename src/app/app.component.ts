bad20510b50f79f8430fb60a2c952ae5d8da13e2
hfidelis <heitorc88@gmail.com>
2023-11-20 00:18:47 -0300

    feat: added Home component and add/remove logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
+  template: `
+    <app-header />
+    <router-outlet></router-outlet>
+    <app-footer />
+  `,
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+  ],
 })
 export class AppComponent {
   title = 'observables';
---
+++ src/app/views/home/home.component.ts
@@ -0,0 +1,121 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Observable } from 'rxjs';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+
+import { SpinnerComponent } from '../../components/spinner/spinner.component';
+import { People } from '../../models/People.model';
+import { PeopleService } from '../../services/people/people.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    SpinnerComponent,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FontAwesomeModule,
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.people$ = this.peopleService.getPeople();
+  }
+
+  peopleService = inject(PeopleService)
+
+  people$!: Observable<People[]>;
+
+  faTrashCan = faTrashCan;
+
+  roleOptions: string[] = [
+    'Developer',
+    'QA Tester',
+    'Product Owner',
+    'Scrum Master',
+    'UX Designer',
+    'UI Designer',
+    'Business Analyst',
+    'Data Analyst',
+    'Data Scientist',
+    'DevOps Engineer',
+    'Cloud Engineer',
+    'Security Engineer',
+  ]
+
+  peopleForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    role: new FormControl('', [Validators.required]),
+  });
+
+  @ViewChild('formDirective')
+  private formDirective: NgForm | undefined;
+
+  get name() {
+    return this.peopleForm.get('name')!;
+  }
+
+  get role() {
+    return this.peopleForm.get('role')!;
+  }
+
+  getNameErrorMessage(): string {
+    if (this.name.hasError('required')) {
+      return 'Insira um nome!';
+    }
+
+    return this.name.hasError('minLength') ? 'Insira um nome de no mín. 3 letras!' : '';
+  }
+
+  getRoleErrorMessage(): string {
+    return this.role.hasError('required') ? 'Insira um cargo!' : '';
+  }
+
+  remove(id: number | string): void {
+    this.peopleService.deletePeople(id)
+                      .subscribe({
+                        next: () => {
+                          this.people$ = this.peopleService.getPeople();
+                        }
+                      })
+
+  }
+
+  onSubmit(): void {
+    console.log('oioi')
+
+    if (this.peopleForm.valid) {
+      console.log(this.name.value, this.role.value)
+      const value: People = {
+        id: '',
+        name: this.name.value,
+        role: this.role.value,
+      }
+
+      this.peopleForm.reset();
+      this.formDirective!.resetForm();
+
+      this.peopleService.addPeople(value)
+                        .subscribe({
+                          next: () => {
+                            this.people$ = this.peopleService.getPeople();
+                          }
+                        })
+    }
+  }
+}
